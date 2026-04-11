d2419cd855da2d3137511285a96d68661fb90c19
JanP1 <janp1krc@gmail.com>
2026-04-11 10:33:41 +0200

    Backend project cleanup
    Removing controller, repository, model and service directories

--- backend/marsapp/src/main/java/com/marsapp/marsapp/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.marsapp.marsapp.repository;
-
-import com.marsapp.marsapp.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<Test, Long> { }
