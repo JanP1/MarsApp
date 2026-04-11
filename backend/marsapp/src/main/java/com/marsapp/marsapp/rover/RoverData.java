3f629588a87ed77236da5a78e70200f1139c497b
JanP1 <janp1krc@gmail.com>
2026-04-11 18:42:29 +0200

    Adding rover API

---
+++ backend/marsapp/src/main/java/com/marsapp/marsapp/rover/RoverData.java
@@ -0,0 +1,52 @@
+package com.marsapp.marsapp.rover;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rover_data")
+public class RoverData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int sol;
+
+    @Column(name = "lat")
+    private int lattitude;
+
+    @Column(name = "lon")
+    private int longitude;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public int getSol() {
+        return sol;
+    }
+    public void setSol(int sol) {
+        this.sol = sol;
+    }
+    public int getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+    public int getLattitude() {
+        return lattitude;
+    }
+    public void setLattitude(int lattitude) {
+        this.lattitude = lattitude;
+    }
+    
+}
---
+++ backend/marsapp/src/main/java/com/marsapp/marsapp/rover/RoverDataService.java
@@ -0,0 +1,47 @@
+package com.marsapp.marsapp.rover;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.marsapp.marsapp.exceptions.ResourceNotFoundException;
+
+@Service
+public class RoverDataService {
+
+    private final RoverDataRepository roverDataRepository;
+
+    public RoverDataService(RoverDataRepository roverDataRepository) {
+        this.roverDataRepository = roverDataRepository;
+    }
+    
+
+    public RoverData getLatestRoverPosition() {
+        return roverDataRepository
+            .findFirstByOrderBySolDesc()
+            .orElseThrow(() -> new ResourceNotFoundException("Rover position for sol not found"));
+    }
+
+    public List<RoverData> getRoverPositionInGivenTimePeriod(int startSol, int endSol) {
+        List<RoverData> roverPositions = roverDataRepository.findBySolBetween(startSol, endSol);
+        
+        if (roverPositions.isEmpty()) {
+            throw new ResourceNotFoundException("Data for last week not found");
+        }
+        
+        return roverPositions;
+    }
+
+    public List<RoverData> getRoverPositionFormDateUntillLatest(int startSol) {
+
+        List<RoverData> roverPositions = roverDataRepository.findBySolGreaterThanEqual(startSol);
+        
+        if (roverPositions.isEmpty()) {
+            throw new ResourceNotFoundException("Data for last week not found");
+        }
+        
+        return roverPositions;
+    }
+
+    
+}
