ee171ed565ea1ddd112e9ceda39062e9e7820bf3
JanP1 <janp1krc@gmail.com>
2026-04-12 18:51:55 +0200

    News API further works

--- backend/marsapp/src/main/java/com/marsapp/marsapp/news/NewsDataController.java
+++ backend/marsapp/src/main/java/com/marsapp/marsapp/news/NewsDataController.java
@@ -1,10 +1,16 @@
 package com.marsapp.marsapp.news;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/rover")
+@RequestMapping("api/news")
 public class NewsDataController {
 
     private final NewsDataService newsDataService;
@@ -12,5 +18,25 @@ public class NewsDataController {
     public NewsDataController(NewsDataService newsDataService) {
         this.newsDataService = newsDataService;
     }
+
+
+    @GetMapping
+    public List<NewsData> getNewsForDate(
+            @RequestParam(name = "date")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+
+        return newsDataService.getNewsForDate(date);
+    }
+
+
+    @GetMapping
+    public List<NewsData> getNewsFromDayTillNow(
+            @RequestParam(name = "from")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from
+    ) {
+        return newsDataService.getNewsFromDayTillNow(from);
+    }
+
 
 }
