7a0d63220a3859276ea971f28aa4aea141a18669
JanP1 <janp1krc@gmail.com>
2026-04-13 11:51:26 +0200

    Changing SQL query to authors

--- db/SQL_queries/news/craete_news_author_map_table.sql
+++ db/SQL_queries/news/craete_news_author_map_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE news_author_map (
     news_id BIGINT REFERENCES news_data(id) ON DELETE CASCADE,
-    author_id BIGINT REFERENCES author(id) ON DELETE CASCADE,
+    author_id BIGINT REFERENCES authors(id) ON DELETE CASCADE,
     PRIMARY KEY (news_id, author_id)
 );
