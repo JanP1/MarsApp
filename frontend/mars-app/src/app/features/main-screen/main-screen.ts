13586f5b075acda0cc9976c86f876c25007e7fff
JanP1 <janp1krc@gmail.com>
2026-04-14 19:04:04 +0200

    Main screen setup

---
+++ frontend/mars-app/src/app/features/main-screen/main-screen.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-screen',
+  imports: [],
+  templateUrl: './main-screen.html',
+  styleUrl: './main-screen.scss',
+})
+export class MainScreen {}
