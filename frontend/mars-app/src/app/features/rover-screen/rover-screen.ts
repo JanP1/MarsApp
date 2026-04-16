b770d8e2c38459e206368e16318c8ecc3bd27685
JanP1 <janp1krc@gmail.com>
2026-04-16 20:38:33 +0200

    Fetching rover position

--- frontend/mars-app/src/app/features/rover-screen/rover-screen.ts
+++ frontend/mars-app/src/app/features/rover-screen/rover-screen.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Rover } from './models/rover.model';
+import { RoverService } from './services/rover.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-rover-screen',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './rover-screen.html',
   styleUrl: './rover-screen.scss',
 })
-export class RoverScreen {}
+export class RoverScreen {
+  roverLastPosition$!: Observable<Rover>;
+
+  constructor(private roverService: RoverService) {}
+
+  ngOnInit(): void {
+    this.roverLastPosition$ = this.roverService.getLatestRoverPosition();
+  }
+
+}
