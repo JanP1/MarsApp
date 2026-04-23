bf5a7320a62074edcf51be3081afd385ae59847f
JanP1 <janp1krc@gmail.com>
2026-04-23 16:21:24 +0200

    Setup for Open Layers Map

---
+++ frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/mars-map-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mars-map-component',
+  imports: [],
+  templateUrl: './mars-map-component.html',
+  styleUrl: './mars-map-component.scss',
+})
+export class MarsMapComponent {}
--- frontend/mars-app/src/app/features/rover-screen/rover-screen.ts
+++ frontend/mars-app/src/app/features/rover-screen/rover-screen.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rover } from './models/rover.model';
 import { RoverService } from './services/rover.service';
-import { AsyncPipe } from '@angular/common';
+import { MarsMapComponent } from './components/mars-map-component/mars-map-component';
 
 @Component({
   selector: 'app-rover-screen',
-  imports: [AsyncPipe],
+  imports: [
+    MarsMapComponent
+  ],
   templateUrl: './rover-screen.html',
   styleUrl: './rover-screen.scss',
 })
