98e594421c89d5f565058719feb4e2c307eb69e5
JanP1 <janp1krc@gmail.com>
2026-04-24 08:41:30 +0200

    Merging frontend

--- frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/mars-map-component.ts
+++ frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/mars-map-component.ts
@@ -21,62 +21,60 @@ export class MarsMapComponent implements OnInit {
     register(proj4);
     const projection = getProjection('urn:ogc:def:crs:EPSG::104905')!;
 
-    const resolutions = [
-      0.703125,
-      0.3515625,
-      0.17578125,
-      0.087890625,
-      0.0439453125,
-      0.02197265625,
-      0.010986328125,
-      0.0054931640625,
-      0.00274658203125,
-      0.001373291015625,
-      0.0006866455078125
+    const baseResolutions = [
+      0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
+      0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125,
+      0.001373291015625, 0.0006866455078125
     ];
 
     const matrixIds = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-
     const curiosityExtent: [number, number, number, number] = [136.2768, -7.2137, 139.3864, -3.5804];
+
+    // Allows the View to zoom on the last resolution
+    // without fetching new TileMatrices
+    const viewResolutions = [...baseResolutions];
+    for (let i = 0; i < 4; i++) {
+        viewResolutions.push(viewResolutions[viewResolutions.length - 1] / 2);
+    }
 
     this.map = new Map({
       target: 'mars-map',
       layers: [
         new TileLayer({
+          maxZoom: 100,
           source: new WMTS({
-            url: "/nasa-trek/tiles/Mars/EQ/curiosity_ctx_mosaic/1.0.0//{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.png",
-
+            url: "/nasa-trek/tiles/Mars/EQ/curiosity_ctx_mosaic/1.0.0//default/default028mm/{TileMatrix}/{TileRow}/{TileCol}.png",
             layer: "curiosity_ctx_mosaic",
             requestEncoding: "REST",
             matrixSet: "default028mm",
             format: "image/png",
             style: "default",
-
             crossOrigin: 'anonymous',
-
             projection: projection,
             tileGrid: new WMTSTileGrid({
               origin: [-180, 90],
-              resolutions: resolutions,
+
+              // Only the mars tile provided resolutions
+              // Otherwise it will try to fetch sth like /14/0/0.png
+              resolutions: baseResolutions,
+
               matrixIds: matrixIds
             }),
-            wrapX: true,
+            wrapX: false,
           }),
         }),
       ],
       view: new View({
         projection: projection,
         center: [137.8, -4.5],
-        resolutions: resolutions,
-        zoom: 8,
 
+        resolutions: viewResolutions,
+
+        zoom: 10,
         extent: curiosityExtent,
-
-        minZoom: 4,
-        maxZoom: 10,
-
         constrainOnlyCenter: false,
-        smoothExtentConstraint: true
+        smoothExtentConstraint: true,
+        maxZoom: viewResolutions.length - 1
       }),
     });
   }
