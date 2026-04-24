4c64298bc89f815c9297752e15904a17db1bdcf6
JanP1 <janp1krc@gmail.com>
2026-04-24 15:54:44 +0200

    Merging frontend changes

--- frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/mars-map-component.ts
+++ frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/mars-map-component.ts
@@ -1,81 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import { get as getProjection } from 'ol/proj';
-import { WMTS } from 'ol/source';
-import WMTSTileGrid from 'ol/tilegrid/WMTS';
-import proj4 from 'proj4';
-import {register} from 'ol/proj/proj4';
+import { Map } from 'ol';
+import { MarsMapService } from './services/mars-map.service';
 
 @Component({
   selector: 'app-mars-map-component',
-  imports: [],
+  standalone: true,
   templateUrl: './mars-map-component.html',
   styleUrl: './mars-map-component.scss',
 })
 export class MarsMapComponent implements OnInit {
+
   map!: Map;
 
+  constructor(private marsMapService: MarsMapService) {}
+
   ngOnInit(): void {
-    proj4.defs("urn:ogc:def:crs:EPSG::104905", "+proj=longlat +a=3396190 +b=3376200 +no_defs");
-    register(proj4);
-    const projection = getProjection('urn:ogc:def:crs:EPSG::104905')!;
+    this.initializeMap();
+  }
 
-    const baseResolutions = [
-      0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
-      0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125,
-      0.001373291015625, 0.0006866455078125
-    ];
-
-    const matrixIds = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    const curiosityExtent: [number, number, number, number] = [136.2768, -7.2137, 139.3864, -3.5804];
-
-    // Allows the View to zoom on the last resolution
-    // without fetching new TileMatrices
-    const viewResolutions = [...baseResolutions];
-    for (let i = 0; i < 4; i++) {
-        viewResolutions.push(viewResolutions[viewResolutions.length - 1] / 2);
-    }
-
+  private initializeMap(): void {
     this.map = new Map({
       target: 'mars-map',
       layers: [
-        new TileLayer({
-          maxZoom: 100,
-          source: new WMTS({
-            url: "/nasa-trek/tiles/Mars/EQ/curiosity_ctx_mosaic/1.0.0//default/default028mm/{TileMatrix}/{TileRow}/{TileCol}.png",
-            layer: "curiosity_ctx_mosaic",
-            requestEncoding: "REST",
-            matrixSet: "default028mm",
-            format: "image/png",
-            style: "default",
-            crossOrigin: 'anonymous',
-            projection: projection,
-            tileGrid: new WMTSTileGrid({
-              origin: [-180, 90],
-
-              // Only the mars tile provided resolutions
-              // Otherwise it will try to fetch sth like /14/0/0.png
-              resolutions: baseResolutions,
-
-              matrixIds: matrixIds
-            }),
-            wrapX: false,
-          }),
-        }),
+        this.marsMapService.getCuriosityCtxLayer(),
+        this.marsMapService.getRoverPosIconLayer()
       ],
-      view: new View({
-        projection: projection,
-        center: [137.8, -4.5],
-
-        resolutions: viewResolutions,
-
-        zoom: 10,
-        extent: curiosityExtent,
-        constrainOnlyCenter: false,
-        smoothExtentConstraint: true,
-        maxZoom: viewResolutions.length - 1
-      }),
+      view: this.marsMapService.createMarsView(),
     });
   }
 }
---
+++ frontend/mars-app/src/app/features/rover-screen/components/mars-map-component/services/mars-map.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from "@angular/core";
+import { Feature, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { get as getProjection, Projection } from 'ol/proj';
+import { WMTS } from 'ol/source';
+import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import proj4 from 'proj4';
+import { register } from 'ol/proj/proj4';
+import VectorLayer from "ol/layer/Vector";
+import { Point } from "ol/geom";
+import Style from "ol/style/Style";
+import Icon from "ol/style/Icon";
+import VectorSource from "ol/source/Vector";
+
+@Injectable({ providedIn: 'root' })
+export class MarsMapService {
+
+  private readonly MARS_CRS = "urn:ogc:def:crs:EPSG::104905";
+
+  private readonly BASE_RESOLUTIONS = [
+    0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
+    0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125,
+    0.001373291015625, 0.0006866455078125
+  ];
+
+  constructor() {
+    proj4.defs(this.MARS_CRS, "+proj=longlat +a=3396190 +b=3376200 +no_defs");
+    register(proj4);
+  }
+
+  getMarsProjection(): Projection {
+    return getProjection(this.MARS_CRS)!;
+  }
+
+  getCuriosityCtxLayer(): TileLayer {
+    const matrixIds = this.BASE_RESOLUTIONS.map((_, i) => i.toString());
+
+    return new TileLayer({
+      className: 'mars-map-tiles',
+      maxZoom: 100,
+      source: new WMTS({
+        url: "/nasa-trek/tiles/Mars/EQ/curiosity_ctx_mosaic/1.0.0//default/default028mm/{TileMatrix}/{TileRow}/{TileCol}.png",
+        layer: "curiosity_ctx_mosaic",
+        requestEncoding: "REST",
+        matrixSet: "default028mm",
+        format: "image/png",
+        style: "default",
+        crossOrigin: 'anonymous',
+
+        projection: this.getMarsProjection(),
+
+        tileGrid: new WMTSTileGrid({
+          origin: [-180, 90],
+          resolutions: this.BASE_RESOLUTIONS,
+          matrixIds: matrixIds
+        }),
+
+        wrapX: false,
+      }),
+    });
+  }
+
+  createMarsView(): View {
+    const curiosityExtent: [number, number, number, number] = [136.2768, -7.2137, 139.3864, -3.5804];
+
+    const viewResolutions = [...this.BASE_RESOLUTIONS];
+    for (let i = 0; i < 4; i++) {
+      viewResolutions.push(viewResolutions[viewResolutions.length - 1] / 2);
+    }
+
+    return new View({
+      projection: this.getMarsProjection(),
+      center: [137.8, -4.5],
+      resolutions: viewResolutions,
+      zoom: 10,
+      extent: curiosityExtent,
+      constrainOnlyCenter: false,
+      smoothExtentConstraint: true,
+      maxZoom: viewResolutions.length - 1
+    });
+  }
+
+
+  // ======= ROVER POSITION ICON =======================
+
+  getRoverPosIconLayer(): VectorLayer {
+    const iconFeature = new Feature({
+      geometry: new Point([
+        137.3817063, // for now hardcoded
+        -4.8142672   // TODO Change later
+      ]),
+      name: 'Null Island',
+      population: 4000,
+      rainfall: 500,
+    });
+
+    const iconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        scale: 0.1,
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        src: '/assets/rover_pointer.png',
+      }),
+    });
+
+    iconFeature.setStyle(iconStyle);
+
+    const vectorSource = new VectorSource({
+      features: [iconFeature],
+    });
+
+    return new VectorLayer({
+      source: vectorSource,
+    });
+  }
+
+
+}
