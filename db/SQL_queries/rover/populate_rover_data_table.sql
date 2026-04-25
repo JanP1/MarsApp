ecc3c543dedd59c9f9260b60bd711c7d95f217a4
JanP1 <janp1krc@gmail.com>
2026-04-25 10:43:07 +0200

    Changing rover table creation and population scripts

--- db/SQL_queries/rover/populate_rover_data_table.sql
+++ db/SQL_queries/rover/populate_rover_data_table.sql
@@ -1,6 +1,8 @@
-INSERT INTO rover_data (sol, lat, lon)
+INSERT INTO rover_data (sol, lat, lon, elevation)
 SELECT 
     s AS sol, 
     (random() * 100)::int AS lat, 
-    (random() * 100)::int AS lon
+    (random() * 100)::int AS lon,
+    (random() * 100)::int AS elevation
+
 FROM generate_series(1, 100) AS s;
