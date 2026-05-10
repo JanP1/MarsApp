80688eee53c55ac72de1ce9ad42d15bc403487a1
JanP1 <janp1krc@gmail.com>
2026-05-10 17:39:06 +0200

    Merginng frontend changes. Organizing header

--- frontend/mars-app/src/app/features/main-screen/main-screen.ts
+++ frontend/mars-app/src/app/features/main-screen/main-screen.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-screen',
   imports: [
-    RouterLink,
   ],
   templateUrl: './main-screen.html',
   styleUrl: './main-screen.scss',
